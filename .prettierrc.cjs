f3a23cd05f3711ffbe3ec72ab567b5680c1f4199
ijquenta <ivanjosuequentavargas@gmail.com>
2024-09-19 09:14:46 -0400

    [UPDATE] Config

---
+++ .prettierrc.cjs
@@ -0,0 +1,18 @@
+module.export = {
+    semi: true,
+    singleQuote: true,
+    tabWidth: 2,
+    useTabs: true,
+    plugins: [
+        require("prettier-plugin-astro")
+    ],
+    overrides: [
+        {
+            files: "**/*astro",
+            options: {
+                parser: "astro"
+            }
+        }
+    ]
+
+}
